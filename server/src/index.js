a584b3b6548e78038f822dd0c7abb7077f850ac0
Tejsvi Bhat <tejsvibhat@gmail.com>
2026-05-22 20:27:20 +0530

    fix wildcard route for express v5
    express v5 uses {*splat} instead of * for catch-all routes

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.use('/api/projects', dashboardRoutes);
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
